a380c7f62ad1237feb42d681a8f4eb156056bf39
Raphael015 <149971212+Raphael015@users.noreply.github.com>
2023-11-26 17:04:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Contador</h1>
+        <p>Contagem: {count}</p>
+        <button onClick={decrement}>Remover</button>
+        <button onClick={increment}>Adicionar</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
